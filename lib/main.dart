eb631c286053d2656a6f446cad1af6d603537a7a
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2023-02-16 21:28:33 +0530

    images and assests

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home()
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
         centerTitle: true,
         backgroundColor: Colors.blue[300],
       ),
       body: Center(
-        child: Text(
-          'Hey There',
-          style: TextStyle(
-            fontSize: 20.9,
-            color: Colors.grey[600],
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.9,
-          ),
-        ),
+        child : Image.asset('assets/fooddrink.jpg')
+        // ,child: Text(
+        //   'Hey There',
+        //   style: TextStyle( 
+        //       fontSize: 20.9,
+        //       color: Colors.grey[600],
+        //       fontWeight: FontWeight.bold,
+        //       letterSpacing: 2.9,
+        //       fontFamily: 'RobotoItalic'),
+        // ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print("hey"),
         child: Text("Click"),
         backgroundColor: Colors.blue[300],
       ),
-    ),
-    
-  ));
- }
+    );
+  }
+}
