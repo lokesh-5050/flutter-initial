bcb26805145cff5d5fdd02981f6b781e85b937a4
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2023-02-16 22:53:58 +0530

    buttons and padding and margins

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home()
-  ));
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatelessWidget {
@@ -17,18 +15,51 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue[300],
       ),
-      body: Center(
-        child : Image.asset('assets/fooddrink.jpg')
-        // ,child: Text(
-        //   'Hey There',
-        //   style: TextStyle( 
-        //       fontSize: 20.9,
-        //       color: Colors.grey[600],
-        //       fontWeight: FontWeight.bold,
-        //       letterSpacing: 2.9,
-        //       fontFamily: 'RobotoItalic'),
-        // ),
-      ),
+      // body: Center(
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal:190 , vertical: 50),
+          margin: EdgeInsets.all(20),
+          child: Text("hey"),
+          color: Colors.grey,
+        ),
+        
+          // child: ElevatedButton(
+          //   onPressed: ()=>{},
+          //   child: Text("hey"),
+          // ),
+          // child: IconButton(
+          //   onPressed: ()=>{},
+          //   icon: Icon(Icons.alarm),
+          //   color: Colors.blue,
+          // ),
+          // child: ElevatedButton.icon(
+          //   style: ElevatedButton.styleFrom(
+          //     backgroundColor: Colors.blue[400]
+          //   ),
+          //   onPressed: () => {},
+          //   icon: Icon(
+          //     Icons.mail,
+          //     color: Colors.white,
+          //   ),
+          //   label: Text("Mail me"),
+          // ),
+          // child : Image.asset('assets/fooddrink.jpg')
+          // child: Icon(
+          //   Icons.sunny,
+          //   color: Colors.blue,
+          //   size: 30,
+          // ),
+
+          // ,child: Text(
+          //   'Hey There',
+          //   style: TextStyle(
+          //       fontSize: 20.9,
+          //       color: Colors.grey[600],
+          //       fontWeight: FontWeight.bold,
+          //       letterSpacing: 2.9,
+          //       fontFamily: 'RobotoItalic'),
+          // ),
+          // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print("hey"),
         child: Text("Click"),
